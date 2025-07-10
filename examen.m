860197ab185899ff07938f8b85f07408db2e9458
Bearserker10029 <kentjianbin@gmail.com>
2025-07-10 16:20:08 -0500

    feat: añadir archivo examen.m con implementaciones de análisis de señales y transformadas

---
+++ examen.m
@@ -0,0 +1,54 @@
+clear all
+close all
+
+num=[2 5 0 -4];
+den=[1 0 -5 0 4];
+[r,p,k]=residue(num,den)
+
+for i=1:length(r)
+    rats(r(i))
+end
+
+for i=1:length(p)
+    rats(p(i))
+end
+%% 
+clear all
+close all
+syms x n 
+
+f= exp(j*x*(n+1))+exp(j*x*(n-1));
+
+int(f,x,-pi/2,pi/2)*1/(4*pi)
+%% 
+clear all
+close all
+syms t n 
+f= (dirac(t+1/2)-dirac(t-1/2))*exp(-j*pi*t*n);
+
+4*int(f,t,-1,1)
+%% 
+clear all
+close all
+
+syms t w
+x=2*(exp(3*j*t)+exp(-j*3*t));
+fourierTransform = fourier(x,t,w)
+
+y=subs(x, t, (2-t)) + subs(x, t, (-2-t));
+
+fourierTransform1 = fourier(y,t,w)
+
+z=subs(x,t,3*t+5);
+
+fourierTransform2 = fourier(z,t,w)
+
+%%
+clear all
+close all
+
+syms n z
+
+x=(1/4)^n;
+
+ztrans(x,n,z)
